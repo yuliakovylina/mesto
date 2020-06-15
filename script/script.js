f5301bec580e1577b7cd47afb3ecbb599ac2376a
Yulia Kovylina <yulianastepanovaa@yandex.ru>
2020-06-15 12:24:39 +0300

    project5 after 1st review

--- script/script.js
+++ script/script.js
@@ -1,26 +1,34 @@
-const popup = document.querySelector(".popup"); //нашли модальное окно
-const button = document.querySelector(".profile__small-button"); //нашли кнопку редактирования профиля
-const close = document.querySelector(".popup__button-close");//нашли кнопку закрытия профиля
-const formElement = popup.querySelector(".form"); //нашли форму
+//нашли модальное окно
+const popupProfile = document.querySelector('.popup_profile'); 
+//нашли кнопку редактирования профиля
+const buttonEditProfile = document.querySelector(".profile__small-button");
+//нашли кнопку закрытия профиля
+const close = document.querySelector(".popup__button-close");
+//нашли форму
+const formElement = popupProfile.querySelector(".form");
 const formElementPic = document.querySelector(".form_pic");
-const formSubmit = popup.querySelector('.form__save'); //форма - нашли кнопку сохранить/создать
-const nameInput = formElement.querySelector(".form__input_name"); //форма - нашли поле ввода имени пользователя
-const jobInput = formElement.querySelector(".form__input_profession");//форма - нашли поле ввода рода деятельности пользователя
+//форма - нашли поле ввода имени пользователя
+const nameInput = formElement.querySelector(".form__input_name");
+//форма - нашли поле ввода рода деятельности пользователя
+const jobInput = formElement.querySelector(".form__input_profession");
 const placeInput = document.querySelector(".form__input_place");
 const linkInput = document.querySelector(".form__input_link");
 const profileTitle = document.querySelector(".profile__title");  
 const profileSubtitle = document.querySelector(".profile__subtitle");
-const popupPicture = document.querySelector(".popup_picture");//находим попап добавления картинок
-const mainButton = document.querySelector(".profile__button");// находим кнопку добавления картинки*кнопка-плюс*
-const closePic = document.querySelector(".popup__button-close_pic"); // нашли кнопку закрытия попапа с картинками
-const elementsContainer = document.querySelector(".elements");//нашли контейнер, в который будем добавлять карточки
-const cardTemplate = document.querySelector('.card-template').content; //нашли темплейт, в котором будем работать
+//находим попап добавления картинок
+const popupPicture = document.querySelector(".popup_cards");
+// находим кнопку добавления картинки*кнопка-плюс*
+const mainButton = document.querySelector(".profile__button");
+// нашли кнопку закрытия попапа с картинками
+const closePic = document.querySelector(".popup__button-close_pic");
+//нашли контейнер, в который будем добавлять карточки 
+const elementsContainer = document.querySelector(".elements");
+//нашли темплейт, в котором будем работать
+const cardTemplate = document.querySelector('.card-template').content;
 const bigPicturePopup = document.getElementById('picture-large');
 const bigPicture = document.querySelector('.popup__item');
 const bigPictureName = document.querySelector('.popup__name');
 const bigPictureCloseButton = document.querySelector('.popup__button-close_enlarge');
-
-
 const initialCards = [
     {
         name: 'Архыз',
@@ -49,54 +57,46 @@ const initialCards = [
 ];
 
 //добавляем карточки из массива на страницу
-function cloneCard(name, link) {
-  const card = cardTemplate.cloneNode(true);//клонируем темплейт, в котором будем работать
-  const cardName = card.querySelector('.card__name'); //присываиваем название
+function addCard(name, link) {
+  //клонируем темплейт, в котором будем работать
+  const card = cardTemplate.cloneNode(true);
+  //присываиваем название
+  const cardName = card.querySelector('.card__name');
   cardName.textContent = name;
-  const cardLink = card.querySelector('.card__image');//присваиваем ссылкe
+  cardName.alt = name;
+  //присваиваем ссылку
+  const cardLink = card.querySelector('.card__image');
   cardLink.src = link;
  
   card.querySelector('.card__like').addEventListener('click', toggleLike);
   card.querySelector('.card__delete').addEventListener('click', cardRemove);
-  cardLink.addEventListener('click', function (evt) {
-    bigPicturePopup.classList.toggle('popup_opened');
+  cardLink.addEventListener('click', bigPicturePopupOpenClose);
+  
+  function bigPicturePopupOpenClose() {
     bigPicture.src = link;
     bigPictureName.textContent = name;
-  });
-  
-  elementsContainer.prepend(card);//добавляем контейнер в DOM
-
+    bigPictureName.alt = name;
+    togglePopup(bigPicturePopup);
+  }
+    return card;
 }
 
 //перебираем содержимое массива
-initialCards.forEach(function (item) { 
-  cloneCard(item.name, item.link);
+initialCards.forEach(function(item) { 
+  elementsContainer.prepend(addCard(item.name, item.link));
 })
 
 //функция открытия попапа добавления карточек
-function popupPictureAdd() {
-  popupPicture.classList.add('popup_opened');
+function popupPictureAddRemove() {
   placeInput.value = "";
   linkInput.value = "";
-  
+  togglePopup(popupPicture); 
 }
-
-//закрываем попап добавления карточки
-function closePicturePopup() {
-  popupPicture.classList.remove('popup_opened');
-}
-
-const buttonLike = document.querySelectorAll('.card__like');
 
 //меняем свойство кнопки like
 function toggleLike(evt) {
-        evt.target.classList.toggle('card__like_clicked');
-  }
-
-function addListener(elem) {
-  elem.addEventListener('click', toggleLike);
+  evt.target.classList.toggle('card__like_clicked');
 }
-buttonLike.forEach(addListener); 
 
 //функция удаления карточки
 function cardRemove(evt) {
@@ -107,12 +107,8 @@ function cardRemove(evt) {
 //добавляем новую карточку
 function formSubmitPictureAdd(evt) {
   evt.preventDefault();
-  
-  const nameInput = document.querySelector('.form__input_place').value;
-  const linkInput = document.querySelector('.form__input_link').value;
-  
-  cloneCard(nameInput, linkInput);
-  closePicturePopup();
+  elementsContainer.prepend(addCard(placeInput.value, linkInput.value));
+  popupPictureAddRemove();
 }
 
 //функция закрытия большого попапа
@@ -120,32 +116,30 @@ function bigPopupClose() {
   bigPicturePopup.classList.toggle('popup_opened');
 }
 
-function popupOpened() { //функция открытия/эакрытия попапа
-    popup.classList.add('popup_opened'); //добавляет/удаляет .popup_opened
-    nameInput.value = profileTitle.textContent //заполняет поля формы ранее заданными значениями
-    jobInput.value = profileSubtitle.textContent
-    
+function togglePopup(elem) {
+  elem.classList.toggle('popup_opened');
 }
 
-//закрытие попапа *редактирование профиля*
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function popupProfileOpened() {
+  nameInput.value = profileTitle.textContent; 
+  jobInput.value = profileSubtitle.textContent;
+  togglePopup(popupProfile);
 }
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  closePopup()
+  popupProfileOpened()
   //нажимаем на "сохранить", данные из формы сохраняются в заголовки, попап закрывается
 }
 
-button.addEventListener('click', popupOpened);//вешаем слушатель на кнопку открытия попапа
-close.addEventListener('click', closePopup);//вешаем слушатель на кнопку закрытия попапа
-formElement.addEventListener('submit', formSubmitHandler);// вешаем слушатель на кнопку сохранить
-                                                            // и она закрывается *submit*Я НЕ МОГУ ПОНЯТЬ ЧТО Я СДЕЛАЛА
-mainButton.addEventListener('click', popupPictureAdd);//слушатель на кнопку плюс, открываем форму создания карточки    
-closePic.addEventListener('click', closePicturePopup); //нажимаем на крестик формы создания карточки-закрываем форму
+//вешаем слушатель на кнопки и другие элементы открытия/закрытия
+buttonEditProfile.addEventListener('click', popupProfileOpened);
+close.addEventListener('click', popupProfileOpened);
+formElement.addEventListener('submit', formSubmitHandler);                                                           
+mainButton.addEventListener('click', popupPictureAddRemove);    
+closePic.addEventListener('click', popupPictureAddRemove); 
 formElementPic.addEventListener('submit',formSubmitPictureAdd );                                        
 bigPictureCloseButton.addEventListener('click', bigPopupClose);
  
