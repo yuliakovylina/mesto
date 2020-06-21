1e4cd08f275df82baed5496a6dfa57ac0141d2ba
Yulia Kovylina <yulianastepanovaa@yandex.ru>
2020-06-21 17:26:43 +0300

    fixing after 1st review -6

--- script/index.js
+++ script/index.js
@@ -1,16 +1,16 @@
 //нашли модальное окно
 const popupProfile = document.querySelector('.popup_profile'); 
 //нашли кнопку редактирования профиля
-const buttonEditProfile = document.querySelector(".profile__small-button");
+const editProfileButton = document.querySelector(".profile__small-button");
 //нашли кнопку закрытия профиля
-const close = document.querySelector(".popup__button-close");
+const closePopupButton = document.querySelector(".popup__button-close_profile");
 //нашли форму
-const formElement = popupProfile.querySelector(".form");
-const formElementPic = document.querySelector(".form_pic");
+const editProfileForm = popupProfile.querySelector(".form_edit");
+const addPictureForm = document.querySelector(".form_pic");
 //форма - нашли поле ввода имени пользователя
-const nameInput = formElement.querySelector(".form__input_name");
+const nameInput = editProfileForm.querySelector(".form__input_name");
 //форма - нашли поле ввода рода деятельности пользователя
-const jobInput = formElement.querySelector(".form__input_profession");
+const jobInput = editProfileForm.querySelector(".form__input_profession");
 const placeInput = document.querySelector(".form__input_place");
 const linkInput = document.querySelector(".form__input_link");
 const profileTitle = document.querySelector(".profile__title");  
@@ -18,9 +18,9 @@ const profileSubtitle = document.querySelector(".profile__subtitle");
 //находим попап добавления картинок
 const popupPicture = document.querySelector(".popup_cards");
 // находим кнопку добавления картинки*кнопка-плюс*
-const mainButton = document.querySelector(".profile__button");
+const plusButton = document.querySelector(".profile__button");
 // нашли кнопку закрытия попапа с картинками
-const closePic = document.querySelector(".popup__button-close_pic");
+const closePictureButton = document.querySelector(".popup__button-close_pic");
 //нашли контейнер, в который будем добавлять карточки 
 const elementsContainer = document.querySelector(".elements");
 //нашли темплейт, в котором будем работать  
@@ -29,32 +29,8 @@ const bigPicturePopup = document.getElementById('picture-large');
 const bigPicture = document.querySelector('.popup__item');
 const bigPictureName = document.querySelector('.popup__name');
 const bigPictureCloseButton = document.querySelector('.popup__button-close_enlarge');
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+const popups = Array.from(document.querySelectorAll(".popup"));
+
 
 //добавляем карточки из массива на страницу
 function addCard(name, link) {
@@ -69,9 +45,8 @@ function addCard(name, link) {
   cardLink.alt = name;
   
   card.querySelector('.card__like').addEventListener('click', toggleLike);
-  card.querySelector('.card__delete').addEventListener('click', cardRemove);
+  card.querySelector('.card__delete').addEventListener('click', removeCard);
   card.querySelector('.card__picture').addEventListener('click', bigPicturePopupOpenClose);
-  //cardLink.addEventListener('click', bigPicturePopupOpenClose);
 
   return card;
 }
@@ -79,12 +54,13 @@ function addCard(name, link) {
 //перебираем содержимое массива
 initialCards.forEach(function(item) { 
   elementsContainer.prepend(addCard(item.name, item.link));
-})
+});
 
 //функция открытия попапа добавления карточек
-function popupPictureAddRemove() {
+function openClosePictureHandler() {
   placeInput.value = "";
   linkInput.value = "";
+  resetError(popupPicture);
   togglePopup(popupPicture); 
 }
 
@@ -94,7 +70,7 @@ function toggleLike(evt) {
 }
 
 //функция удаления карточки
-function cardRemove(evt) {
+function removeCard(evt) {
   const card = evt.target.closest('.card');
   card.remove();
 }
@@ -111,19 +87,19 @@ function bigPicturePopupOpenClose (evt) {
   const item = evt.target;
   bigPicture.src = item.src;
   bigPictureName.textContent = item.alt; 
-  console.log(name);
   bigPictureName.alt = name; 
+  
   togglePopup(bigPicturePopup); 
 }
 
 //открытие модального окна
-function togglePopup(elem) {
-  elem.classList.toggle('popup_opened');
-  clickAndEscape(elem);
+function togglePopup(popup) {
+  popup.classList.toggle('popup_opened');
+  clickAndEscapeListeners(popup);
 }
 
 //открытие/закрытие попапа-редактирования профиля
-function popupProfileOpened() {
+function openProfileForm() {
   nameInput.value = profileTitle.textContent; 
   jobInput.value = profileSubtitle.textContent;
   togglePopup(popupProfile);
@@ -133,11 +109,11 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  togglePopup(popupProfile);
+  toggle(popupProfile);
   //нажимаем на "сохранить", данные из формы сохраняются в заголовки, попап закрывается
 }
 
-function clickAndEscape(elem) {
+function clickAndEscapeListeners(elem) {
   document.addEventListener('keydown', function (evt) {
     if (evt.key === "Escape") {
       elem.classList.remove('popup_opened');
@@ -151,14 +127,20 @@ function clickAndEscape(elem) {
   });
 }
 
+function closePopup(){
+  document.querySelector('.popup_opened').classList.remove('popup_opened');
+}
+
+
+
 //вешаем слушатель на кнопки и другие элементы открытия/закрытия
-buttonEditProfile.addEventListener('click', popupProfileOpened);
-close.addEventListener('click', () => togglePopup(popupProfile));
-formElement.addEventListener('submit', formSubmitHandler);                                                           
-mainButton.addEventListener('click', popupPictureAddRemove);    
-closePic.addEventListener('click', () => togglePopup(popupPicture)); 
-formElementPic.addEventListener('submit',formSubmitPictureAdd );                                        
-bigPictureCloseButton.addEventListener('click', () => togglePopup(bigPicturePopup));
+editProfileButton.addEventListener('click', openProfileForm);
+closePopupButton.addEventListener('click', closePopup);
+editProfileForm.addEventListener('submit', formSubmitHandler);                                                           
+plusButton.addEventListener('click', openClosePictureHandler);    
+closePictureButton.addEventListener('click', closePopup); 
+addPictureForm.addEventListener('submit',formSubmitPictureAdd );                                        
+bigPictureCloseButton.addEventListener('click', closePopup);
  
   
 
